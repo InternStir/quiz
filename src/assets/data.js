479d24aa0dbf847cf9e7ca4f6d7e51e50102311c
InternStir <intern@stireducation.org>
2024-02-21 10:32:47 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+      question: "what was the combined  role received for tdc and Mentor Teacher?",
+      option1: "2020",
+      option2: "3100",
+      option3: "3580",
+      option4: "4796",
+      ans: 3,
+    },
+    {
+      question: "Which school id had the highest number of responses?",
+      option1: "1617028",
+      option2: "1104142",
+      option3: "2675134",
+      option4: "2675177",
+      ans: 2,
+    },
+    {
+      question: "With TDC ROLE howmnay responses where from south east district?",
+      option1: "201",
+      option2: "208",
+      option3: "234",
+      option4: "677",
+      ans: 1,
+    },
+
+  ];
